af6684b396b967f787b04064a5a50a782e80fc37
bumyoung001 <bum_young@nate.com>
2022-11-14 17:47:56 +0900

    공통 모듈 분리

---
+++ js/common.js
@@ -0,0 +1,20 @@
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input')
+
+searchEl.addEventListener('click', function() {
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function() {
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합 검색');
+})
+
+searchInputEl.addEventListener('blur', function() {
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+})
+
+// document.querySelector('.this-year').append(new Date().getFullYear())
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
